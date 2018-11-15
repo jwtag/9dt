6a33e018148c10cc23b307f1a3087dd289d28e5b
John Taggart <36749268+jwtag@users.noreply.github.com>
2018-11-14 19:27:25 -0800

    initial commit

---
+++ src/game.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Main game class for 9dt implementation.
+ * @author jwtag
+ *
+ */
+public class game {
+
+	private static int[][] board;  // Stores the board.
+	
+	public static void main(String[] args) {
+		board = new int[4][4];
+		Scanner input = new Scanner(System.in);
+		
+	}
+}
